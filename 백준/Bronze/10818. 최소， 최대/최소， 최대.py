21fef01493a14d249a109fd576510bf58c8049bd
LEE JUNSEO <ejrqlddl1@sch.ac.kr>
2026-04-08 13:48:22 +0900

    [Bronze III] Title: 최소, 최대, Time: 352 ms, Memory: 129196 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,4 +1,13 @@
 N = int(input())
 l = list(map(int, input().split()))
 
-print(min(l), max(l))
+max = l[0]
+min = l[0]
+
+for i in l[1:]:
+    if i > max:
+        max = i
+    elif i < min:
+        min = i
+        
+print(min, max)
