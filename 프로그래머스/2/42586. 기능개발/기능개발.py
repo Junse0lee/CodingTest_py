8491024bdb35bcca1b34df312c51dcd13e9ab1a8
LEE JUNSEO <ejrqlddl1@sch.ac.kr>
2026-04-30 14:32:12 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 9.27 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,22 @@
+import math
+
+def solution(progresses, speeds):
+    res = []
+    count = 0
+    max_day = 0  # 현재 배포 그룹의 최대 소요일
+
+    for i in range(len(progresses)):
+        days = math.ceil((100 - progresses[i]) / speeds[i])
+
+        if count > 0 and max_day < days:
+            res.append(count)
+            count = 0
+            max_day = 0
+
+        max_day = max(max_day, days)
+        count += 1
+
+    if count > 0:
+        res.append(count)
+
+    return res
