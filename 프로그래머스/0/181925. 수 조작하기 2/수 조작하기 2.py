065823183fa19ee2f368636505b0bf981336f107
LEE JUNSEO <ejrqlddl1@sch.ac.kr>
2026-05-11 11:22:25 +0900

    [level 0] Title: 수 조작하기 2, Time: 8.16 ms, Memory: 13.2 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -3,15 +3,8 @@
 
 def solution(numLog):
     answer = ''
-    for i in range(len(numLog)-1):
-        value_num = numLog[i+1] - numLog[i]
-        if value_num == 1:
-            answer += "w"
-        elif value_num == -1:
-            answer += "s"
-        elif value_num == 10:
-            answer += "d"
-        elif value_num == -10:
-            answer += "a"
+    joystick = dict(zip([1, -1, 10, -10], ["w", "s", "d", "a"]))
+    for i in range(1, len(numLog)):
+        answer += joystick[numLog[i] - numLog[i-1]]
         
     return answer
